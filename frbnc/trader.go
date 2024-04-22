7c3ce449ba2a37e87e530d0fa1d4db20e6417f96
dwd <wenyi173@gmail.com>
2024-04-22 20:53:03 +0800

    add SpFuTrader

--- frbnc/trader.go
+++ frbnc/trader.go
@@ -1,12 +1,41 @@
 package frbnc
 
-import "github.com/dwdwow/cex"
+import (
+	"github.com/dwdwow/cex"
+	"github.com/dwdwow/cex/bnc"
+)
 
-type TraderParams struct {
+type SpFuTraderParams struct {
 	SpPair cex.Pair
 	FuPair cex.Pair
 	SpSide cex.OrderSide
 	FuSide cex.OrderSide
 	SpQty  float64
 	FuQty  float64
+}
+
+type SpFuTraderStatus string
+
+const ()
+
+type SpFuTrader struct {
+	status SpFuTraderStatus
+
+	user *bnc.User
+
+	Params SpFuTraderParams
+
+	SpOrders []*cex.Order
+	FuOrders []*cex.Order
+}
+
+func NewSpFuTrader(user *bnc.User, params SpFuTraderParams) *SpFuTrader {
+	return &SpFuTrader{
+		user:   user,
+		Params: params,
+	}
+}
+
+func (t *SpFuTrader) trade() {
+
 }
