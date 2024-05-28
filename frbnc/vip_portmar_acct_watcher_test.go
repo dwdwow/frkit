e9cc5a029681badfabcfa2df17959005b27baf28
dwd <wenyi173@gmail.com>
2024-05-28 15:18:02 +0800

    add TestNewVIPPortmarAcctWatcher

--- frbnc/vip_portmar_acct_watcher_test.go
+++ frbnc/vip_portmar_acct_watcher_test.go
@@ -1,1 +1,25 @@
 package frbnc
+
+import (
+	"testing"
+
+	"github.com/dwdwow/cex"
+	"github.com/dwdwow/cex/bnc"
+	"github.com/dwdwow/props"
+)
+
+func TestNewVIPPortmarAcctWatcher(t *testing.T) {
+	keys, err := cex.ReadApiKey()
+	props.PanicIfNotNil(err)
+	key, ok := keys["HUANGYAN"]
+	if !ok {
+		panic("not ok")
+	}
+	watcher := NewVIPPortmarAcctWatcher(bnc.NewUser(key.ApiKey, key.SecretKey), nil)
+	err = watcher.Start()
+	props.PanicIfNotNil(err)
+	c := watcher.Sub()
+	for {
+		props.PrintlnIndent(<-c)
+	}
+}
