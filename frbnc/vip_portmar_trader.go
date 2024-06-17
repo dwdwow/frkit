8d59e0f1c810e4131fac91c18468fae74f6ccb52
dwd <wenyi173@gmail.com>
2024-06-17 21:04:52 +0800

    add func: VIPPortmarMarketTraderFunc

--- frbnc/vip_portmar_trader.go
+++ frbnc/vip_portmar_trader.go
@@ -9,213 +9,224 @@ import (
 )
 
 type VIPPortmarPosStatus string
+type VIPPortmarOrderStatus string
 
 const (
-	VIPPortmarPosStatusNone        VIPPortmarPosStatus = "NONE"
-	VIPPortmarPosStatusNew         VIPPortmarPosStatus = "NEW"
-	VIPPortmarPosStatusFuOpening   VIPPortmarPosStatus = "FU_OPENING"
-	VIPPortmarPosStatusSpOpening   VIPPortmarPosStatus = "SP_OPENING"
-	VIPPortmarPosStatusReFuOpening VIPPortmarPosStatus = "RE_FU_OPENING"
-	VIPPortmarPosStatusFuOpened    VIPPortmarPosStatus = "FU_OPENED"
-	VIPPortmarPosStatusSpOpened    VIPPortmarPosStatus = "SP_OPENED"
-	VIPPortmarPosStatusReFuOpened  VIPPortmarPosStatus = "RE_FU_OPENED"
-	VIPPortmarPosStatusFuFailed    VIPPortmarPosStatus = "FU_FAILED"
-	VIPPortmarPosStatusSpFailed    VIPPortmarPosStatus = "SP_FAILED"
-	VIPPortmarPosStatusReFuFailed  VIPPortmarPosStatus = "RE_FU_FAILED"
+	VIPPortmarPosStatusNone             VIPPortmarPosStatus = ""
+	VIPPortmarPosStatusNew              VIPPortmarPosStatus = "NEW"
+	VIPPortmarPosStatusFuOpening        VIPPortmarPosStatus = "FU_OPENING"
+	VIPPortmarPosStatusSpOpening        VIPPortmarPosStatus = "SP_OPENING"
+	VIPPortmarPosStatusReFuOpening      VIPPortmarPosStatus = "RE_FU_OPENING"
+	VIPPortmarPosStatusFuOpened         VIPPortmarPosStatus = "FU_OPENED"
+	VIPPortmarPosStatusSpOpened         VIPPortmarPosStatus = "SP_OPENED"
+	VIPPortmarPosStatusReFuOpened       VIPPortmarPosStatus = "RE_FU_OPENED"
+	VIPPortmarPosStatusFuFailed         VIPPortmarPosStatus = "FU_FAILED"
+	VIPPortmarPosStatusSpFailed         VIPPortmarPosStatus = "SP_FAILED"
+	VIPPortmarPosStatusReFuFailed       VIPPortmarPosStatus = "RE_FU_FAILED"
+	VIPPortmarPosStatusFuWaiterFailed   VIPPortmarPosStatus = "FU_WAITER_FAILED"
+	VIPPortmarPosStatusSpWaiterFailed   VIPPortmarPosStatus = "SP_WAITER_FAILED"
+	VIPPortmarPosStatusReFuWaiterFailed VIPPortmarPosStatus = "RE_WAITER_FU_FAILED"
+
+	VIPPortmarOrderStatusNone         VIPPortmarOrderStatus = ""
+	VIPPortmarOrderStatusOpening      VIPPortmarOrderStatus = "OPENING"
+	VIPPortmarOrderStatusOpened       VIPPortmarOrderStatus = "OPENED"
+	VIPPortmarOrderStatusFailed       VIPPortmarOrderStatus = "FAILED"
+	VIPPortmarOrderStatusWaiterFailed VIPPortmarOrderStatus = "WAITER_FAILED"
 )
 
+type VIPPortmarOrd struct {
+	Order  cex.Order
+	Status VIPPortmarOrderStatus
+	Err    error
+}
+
 type VIPPortmarPosMsg struct {
-	Status  VIPPortmarPosStatus
-	FuOrd   cex.Order
-	SpOrd   cex.Order
-	ReFuOrd cex.Order
-	Err     error
+	FuOrd   VIPPortmarOrd
+	SpOrd   VIPPortmarOrd
+	ReFuOrd VIPPortmarOrd
+	Errs    []error
+}
+
+func (v VIPPortmarPosMsg) Status() VIPPortmarPosStatus {
+	switch v.ReFuOrd.Status {
+	case VIPPortmarOrderStatusOpened:
+		return VIPPortmarPosStatusReFuOpened
+	case VIPPortmarOrderStatusOpening:
+		return VIPPortmarPosStatusReFuOpening
+	case VIPPortmarOrderStatusFailed:
+		return VIPPortmarPosStatusReFuFailed
+	case VIPPortmarOrderStatusWaiterFailed:
+		return VIPPortmarPosStatusReFuWaiterFailed
+	}
+
+	switch v.SpOrd.Status {
+	case VIPPortmarOrderStatusOpened:
+		return VIPPortmarPosStatusSpOpened
+	case VIPPortmarOrderStatusOpening:
+		return VIPPortmarPosStatusSpOpening
+	case VIPPortmarOrderStatusFailed:
+		return VIPPortmarPosStatusSpFailed
+	case VIPPortmarOrderStatusWaiterFailed:
+		return VIPPortmarPosStatusSpWaiterFailed
+	}
+
+	switch v.FuOrd.Status {
+	case VIPPortmarOrderStatusOpening:
+		return VIPPortmarPosStatusFuOpening
+	case VIPPortmarOrderStatusOpened:
+		return VIPPortmarPosStatusFuOpened
+	case VIPPortmarOrderStatusFailed:
+		return VIPPortmarPosStatusFuFailed
+	case VIPPortmarOrderStatusWaiterFailed:
+		return VIPPortmarPosStatusFuWaiterFailed
+	}
+	return VIPPortmarPosStatusNone
 }
 
 type VIPPortmarPosMsger struct {
 	mux       sync.RWMutex
-	Status    VIPPortmarPosStatus
-	LatestMsg VIPPortmarPosMsg
-	MsgChan   chan VIPPortmarPosMsg
+	latestMsg VIPPortmarPosMsg
+	chMsg     chan VIPPortmarPosMsg
 }
 
-func (v *VIPPortmarPosMsger) SetStatus(status VIPPortmarPosStatus) {
+func (v *VIPPortmarPosMsger) SendMsg(msg VIPPortmarPosMsg) {
 	v.mux.Lock()
-	v.Status = status
-	v.mux.Unlock()
-}
-
-func (v *VIPPortmarPosMsger) GetStatus() VIPPortmarPosStatus {
-	v.mux.RLock()
-	defer v.mux.RUnlock()
-	return v.Status
-}
-
-func (v *VIPPortmarPosMsger) SetLatestMsg(msg VIPPortmarPosMsg) {
-	v.mux.Lock()
-	v.LatestMsg = msg
-	v.mux.Unlock()
+	defer v.mux.Unlock()
+	v.latestMsg = msg
+	v.chMsg <- msg
 }
 
 func (v *VIPPortmarPosMsger) GetLatestMsg() VIPPortmarPosMsg {
 	v.mux.RLock()
 	defer v.mux.RUnlock()
-	return v.LatestMsg
+	return v.latestMsg
 }
 
-func VIPPortmarPosTrader(ctx context.Context, user *bnc.User, spSide cex.OrderSide, isCM bool, spPair cex.Pair, spQty float64, fuPair cex.Pair, fuQty float64) *VIPPortmarPosMsger {
+func (v *VIPPortmarPosMsger) Status() VIPPortmarPosStatus {
+	v.mux.RLock()
+	defer v.mux.RUnlock()
+	return v.latestMsg.Status()
+}
+
+func VIPPortmarMarketTraderFunc(ctx context.Context, user *bnc.User, pair cex.Pair, trader cex.MarketTraderFunc, qty float64) (ord VIPPortmarOrd, err error) {
+	ord.Status = VIPPortmarOrderStatusOpening
+
+	_, oriOrd, reqErr := trader(pair.Asset, pair.Quote, qty)
+
+	if oriOrd != nil {
+		ord.Order = *oriOrd
+	}
+
+	if reqErr.IsNotNil() {
+		ord.Status = VIPPortmarOrderStatusFailed
+		ord.Err = reqErr.Err
+		err = reqErr.Err
+		return
+	}
+
+	reqErr = <-user.WaitOrder(ctx, oriOrd)
+
+	if reqErr.IsNotNil() {
+		ord.Status = VIPPortmarOrderStatusWaiterFailed
+		ord.Err = reqErr.Err
+		err = reqErr.Err
+		return
+	}
+
+	if oriOrd != nil {
+		ord.Order = *oriOrd
+	}
+
+	ord.Status = VIPPortmarOrderStatusOpened
+
+	return
+}
+
+type VIPPortmarPosTraderParams struct {
+	User   *bnc.User
+	SpSide cex.OrderSide
+	IsCM   bool
+	SpPair cex.Pair
+	SpQty  float64
+	FuPair cex.Pair
+	FuQty  float64
+}
+
+func VIPPortmarPosTrader(ctx context.Context, params VIPPortmarPosTraderParams) *VIPPortmarPosMsger {
 
 	var spFunc, fuFunc, reFuFunc cex.MarketTraderFunc
 
-	if spSide == cex.OrderSideBuy {
-		spFunc = user.NewSpotMarketBuyOrder
-		if isCM {
-			fuFunc = user.NewFuturesMarketSellCMOrder
-			reFuFunc = user.NewFuturesMarketBuyCMOrder
+	if params.SpSide == cex.OrderSideBuy {
+		spFunc = params.User.NewSpotMarketBuyOrder
+		if params.IsCM {
+			fuFunc = params.User.NewFuturesMarketSellCMOrder
+			reFuFunc = params.User.NewFuturesMarketBuyCMOrder
 		} else {
-			fuFunc = user.NewFuturesMarketSellOrder
-			reFuFunc = user.NewFuturesMarketBuyOrder
+			fuFunc = params.User.NewFuturesMarketSellOrder
+			reFuFunc = params.User.NewFuturesMarketBuyOrder
 		}
 	} else {
-		spFunc = user.NewSpotMarketSellOrder
-		if isCM {
-			fuFunc = user.NewFuturesMarketBuyCMOrder
-			reFuFunc = user.NewFuturesMarketSellCMOrder
+		spFunc = params.User.NewSpotMarketSellOrder
+		if params.IsCM {
+			fuFunc = params.User.NewFuturesMarketBuyCMOrder
+			reFuFunc = params.User.NewFuturesMarketSellCMOrder
 		} else {
-			fuFunc = user.NewFuturesMarketBuyOrder
-			reFuFunc = user.NewFuturesMarketSellOrder
+			fuFunc = params.User.NewFuturesMarketBuyOrder
+			reFuFunc = params.User.NewFuturesMarketSellOrder
 		}
 	}
 
 	msger := &VIPPortmarPosMsger{
-		Status:  VIPPortmarPosStatusNew,
-		MsgChan: make(chan VIPPortmarPosMsg, 3),
+		chMsg: make(chan VIPPortmarPosMsg, 6),
 	}
 
 	go func() {
-		// must trade future firstly
-
-		msger.SetStatus(VIPPortmarPosStatusFuOpening)
-
-		_, fuOrd, reqErr := fuFunc(fuPair.Asset, fuPair.Quote, fuQty)
-
 		var msg VIPPortmarPosMsg
 
-		if fuOrd != nil {
-			msg.FuOrd = *fuOrd
-		}
+		// must trade future firstly
 
-		if reqErr.IsNotNil() {
-			msg.Status = VIPPortmarPosStatusFuFailed
-			msg.Err = reqErr.Err
-			msger.SetStatus(VIPPortmarPosStatusFuFailed)
-			msger.SetLatestMsg(msg)
-			msger.MsgChan <- msg
+		msg.FuOrd.Status = VIPPortmarOrderStatusOpening
+		msger.SendMsg(msg)
+
+		fuOrd, err := VIPPortmarMarketTraderFunc(ctx, params.User, params.FuPair, fuFunc, params.FuQty)
+
+		msg.FuOrd = fuOrd
+
+		if err != nil {
+			msg.Errs = append(msg.Errs, err)
+			msger.SendMsg(msg)
 			return
 		}
 
-		chReqErr := user.WaitOrder(ctx, fuOrd)
-
-		reqErr = <-chReqErr
-
-		msg.FuOrd = *fuOrd
-
-		if reqErr.IsNotNil() {
-			msg.Status = VIPPortmarPosStatusFuFailed
-			msg.Err = reqErr.Err
-			msger.SetStatus(VIPPortmarPosStatusFuFailed)
-			msger.SetLatestMsg(msg)
-			msger.MsgChan <- msg
-			return
-		}
-
-		msg.Status = VIPPortmarPosStatusFuOpened
-		msg.Err = reqErr.Err
-		msger.SetStatus(VIPPortmarPosStatusFuOpened)
-		msger.SetLatestMsg(msg)
-		msger.MsgChan <- msg
+		msger.SendMsg(msg)
 
 		// trade spot
 
-		msger.SetStatus(VIPPortmarPosStatusSpOpening)
+		msg.SpOrd.Status = VIPPortmarOrderStatusOpening
+		msger.SendMsg(msg)
 
-		_, spOrd, reqErr := spFunc(spPair.Asset, spPair.Quote, spQty)
+		spOrd, err := VIPPortmarMarketTraderFunc(ctx, params.User, params.SpPair, spFunc, params.SpQty)
 
-		if spOrd != nil {
-			msg.SpOrd = *spOrd
-		}
+		msg.SpOrd = spOrd
 
-		if reqErr.IsNotNil() {
-			msg.Status = VIPPortmarPosStatusSpFailed
-			msg.Err = reqErr.Err
-			msger.SetStatus(VIPPortmarPosStatusSpFailed)
-			msger.SetLatestMsg(msg)
-			msger.MsgChan <- msg
+		if err != nil {
+			msg.Errs = append(msg.Errs, err)
+			msger.SendMsg(msg)
+			if spOrd.Status == VIPPortmarOrderStatusWaiterFailed {
+				return
+			}
+			reFuOrd, err := VIPPortmarMarketTraderFunc(ctx, params.User, params.FuPair, reFuFunc, params.FuQty)
+			msg.ReFuOrd = reFuOrd
+			if err != nil {
+				msg.Errs = append(msg.Errs, err)
+				msger.SendMsg(msg)
+				return
+			}
+			msger.SendMsg(msg)
 			return
 		}
 
-		chReqErr = user.WaitOrder(ctx, spOrd)
-
-		reqErr = <-chReqErr
-
-		msg.SpOrd = *spOrd
-
-		if reqErr.IsNotNil() {
-			msg.Status = VIPPortmarPosStatusSpFailed
-			msg.Err = reqErr.Err
-			msger.SetStatus(VIPPortmarPosStatusSpFailed)
-			msger.SetLatestMsg(msg)
-			msger.MsgChan <- msg
-
-			// reverse future position
-
-			msger.SetStatus(VIPPortmarPosStatusReFuOpening)
-
-			_, reFuOrd, reqErr := reFuFunc(fuPair.Asset, fuPair.Quote, fuQty)
-
-			if reFuOrd != nil {
-				msg.ReFuOrd = *reFuOrd
-			}
-
-			if reqErr.IsNotNil() {
-				msg.Status = VIPPortmarPosStatusReFuFailed
-				msg.Err = reqErr.Err
-				msger.SetStatus(VIPPortmarPosStatusReFuFailed)
-				msger.SetLatestMsg(msg)
-				msger.MsgChan <- msg
-				return
-			}
-
-			chReqErr = user.WaitOrder(ctx, reFuOrd)
-
-			reqErr = <-chReqErr
-
-			msg.ReFuOrd = *reFuOrd
-
-			if reqErr.IsNotNil() {
-				msg.Status = VIPPortmarPosStatusReFuFailed
-				msg.Err = reqErr.Err
-				msger.SetStatus(VIPPortmarPosStatusReFuFailed)
-				msger.SetLatestMsg(msg)
-				msger.MsgChan <- msg
-				return
-			}
-
-			msg.Status = VIPPortmarPosStatusReFuOpened
-			msg.Err = reqErr.Err
-			msger.SetStatus(VIPPortmarPosStatusReFuOpened)
-			msger.SetLatestMsg(msg)
-			msger.MsgChan <- msg
-
-			return
-		}
-
-		msg.Status = VIPPortmarPosStatusSpOpened
-		msg.Err = reqErr.Err
-		msger.SetStatus(VIPPortmarPosStatusSpOpened)
-		msger.SetLatestMsg(msg)
-		msger.MsgChan <- msg
-
+		msger.SendMsg(msg)
 	}()
 
 	return msger
